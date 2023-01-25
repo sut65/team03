258dd0dbee3371e1ec7e0aabad591475ec198599
Pratchaya Saenphoi <B6311636@g.sut.ac.th>
2023-01-25 14:57:56 +0700

    Add UI for Booking - close #125

--- frontend/src/components/Booking/services/BookingHttpClientService.tsx
+++ frontend/src/components/Booking/services/BookingHttpClientService.tsx
@@ -1,4 +1,4 @@
-import { BookingsInterface } from "../../../models/IBooking";
+import { BookingsInterface, BrachsInterface } from "../../../models/IBooking";
 
 
 const apiUrl = "http://localhost:8080";
@@ -10,24 +10,65 @@ const apiUrl = "http://localhost:8080";
 // r.POST("/bookings", booking.CreateBooking)
 // r.PATCH("/bookings", booking.UpdateBooking)
 // r.DELETE("/bookings/:id", booking.DeleteBooking)
-// // ---Branch---
-// r.GET("/branchs", booking.ListBranchs)
-// r.GET("/branch/:id", booking.GetBranch)
-// r.POST("/branchs", booking.CreateBranch)
-// r.PATCH("/branchs", booking.UpdateBranch)
-// r.DELETE("/branchs/:id", booking.DeleteBranch)
-// //=================================================== Booking Routes
+
+async function GetCustomerByUID() {
+    let uid = localStorage.getItem('user');
+    const requestOptions = {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+        },
+    }
+
+    let res = await fetch(`${apiUrl}/customer/${uid}`, requestOptions)
+        .then((response) => response.json())
+        .then((res) => {
+            if (res.data) {
+                return res.data;
+            } else {
+                return false;
+            }
+        });
+
+    return res;
+}
+
 //List Booking
 async function GetBookings() {
     const requestOptions = {
         method: "GET",
-        //   headers: {
-        //     Authorization: `Bearer ${localStorage.getItem("token")}`,
-        //     "Content-Type": "application/json",
-        //   },
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+        },
     };
 
     let res = await fetch(`${apiUrl}/bookings`, requestOptions)
+        .then((response) => response.json())
+        .then((res) => {
+            if (res.data) {
+                return res.data;
+            } else {
+                return false;
+            }
+        });
+
+    return res;
+}
+
+//Get Booking
+async function GetBooking(data: BookingsInterface) {
+    let b_id = data.ID;
+    const requestOptions = {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+        },
+    };
+
+    let res = await fetch(`${apiUrl}/booking/${b_id}`, requestOptions)
         .then((response) => response.json())
         .then((res) => {
             if (res.data) {
@@ -45,10 +86,10 @@ async function GetBookingsBYUID() {
     let uid = localStorage.getItem('uid');
     const requestOptions = {
         method: "GET",
-        // headers: {
-        //     Authorization: `Bearer ${localStorage.getItem("token")}`,
-        //     "Content-Type": "application/json",
-        // },
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+        },
     };
 
     let res = await fetch(`${apiUrl}/bookings/user/${uid}`, requestOptions)
@@ -68,37 +109,14 @@ async function GetBookingsBYUID() {
 async function Bookings(data: BookingsInterface) {
     const requestOptions = {
         method: "POST",
-        // headers: {
-        //     Authorization: `Bearer ${localStorage.getItem("token")}`,
-        //     "Content-Type": "application/json",
-        // },
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+        },
         body: JSON.stringify(data),
     };
 
     let res = await fetch(`${apiUrl}/bookings`, requestOptions)
-        .then((response) => response.json())
-        .then((res) => {
-            if (res.data) {
-                return res.data;
-            } else {
-                return false;
-            }
-        });
-
-    return res;
-}
-
-//GET List Branchs
-async function GetBranchs() {
-    const requestOptions = {
-        method: "GET",
-        //   headers: {
-        //     Authorization: `Bearer ${localStorage.getItem("token")}`,
-        //     "Content-Type": "application/json",
-        //   },
-    };
-
-    let res = await fetch(`${apiUrl}/branchs`, requestOptions)
         .then((response) => response.json())
         .then((res) => {
             if (res.data) {
@@ -116,13 +134,13 @@ async function DeleteBooking(data: BookingsInterface) {
     let booking_id = data.ID;
     const requestOptions = {
         method: "DELETE",
-        //   headers: {
-        //     Authorization: `Bearer ${localStorage.getItem("token")}`,
-        //     "Content-Type": "application/json",
-        //   },
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+        },
         body: JSON.stringify(data),
     }
-    
+
     let res = await fetch(`${apiUrl}/bookings/${booking_id}`, requestOptions)
         .then((response) => response.json())
         .then((res) => {
@@ -140,10 +158,10 @@ async function DeleteBooking(data: BookingsInterface) {
 async function UppdateBooking(data: BookingsInterface) {
     const requestOptions = {
         method: "PATCH",
-        //   headers: {
-        //     Authorization: `Bearer ${localStorage.getItem("token")}`,
-        //     "Content-Type": "application/json",
-        //   },
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+        },
         body: JSON.stringify(data),
     }
 
@@ -160,10 +178,95 @@ async function UppdateBooking(data: BookingsInterface) {
     return res;
 }
 
+// // ---Branch---
+// r.GET("/branchs", booking.ListBranchs)
+// r.GET("/branch/:id", booking.GetBranch)
+// r.POST("/branchs", booking.CreateBranch)
+// r.PATCH("/branchs", booking.UpdateBranch)
+// r.DELETE("/branchs/:id", booking.DeleteBranch)
+// //=================================================== Booking Routes
+
+//GET List Branchs
+async function GetBranchs() {
+    const requestOptions = {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+        },
+    };
+
+    let res = await fetch(`${apiUrl}/branchs`, requestOptions)
+        .then((response) => response.json())
+        .then((res) => {
+            if (res.data) {
+                return res.data;
+            } else {
+                return false;
+            }
+        });
+
+    return res;
+}
+
+//GET Get Branchs
+async function GetBranch(data: BrachsInterface) {
+    let br_id = data.ID;
+    const requestOptions = {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+        },
+    };
+
+    let res = await fetch(`${apiUrl}/branch/${br_id}`, requestOptions)
+        .then((response) => response.json())
+        .then((res) => {
+            if (res.data) {
+                return res.data;
+            } else {
+                return false;
+            }
+        });
+
+    return res;
+}
+
+//POST Create Branchs
+async function Branchs() {
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+        },
+    };
+
+    let res = await fetch(`${apiUrl}/branchs`, requestOptions)
+        .then((response) => response.json())
+        .then((res) => {
+            if (res.data) {
+                return res.data;
+            } else {
+                return false;
+            }
+        });
+
+    return res;
+}
+
 export {
+    GetCustomerByUID,
+
     Bookings,
     GetBookings,
-    DeleteBooking,
+    GetBooking,
     GetBookingsBYUID, //****Special get */
+    DeleteBooking,
+    UppdateBooking,
+
+    Branchs,
     GetBranchs,
+    GetBranch,
 };
