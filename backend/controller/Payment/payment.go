64fb04007b3494a8bb1a2e256eb32ae119fa6228
Henlies <B6311032@g.sut.ac.th>
2023-01-28 09:53:47 +0700

    Add list fk for combobox - close #150

--- backend/controller/Payment/payment.go
+++ backend/controller/Payment/payment.go
@@ -13,57 +13,49 @@ func CreatePayment(c *gin.Context) {
 	var customer entity.Customer
 	var paymentmethod entity.PaymentMethod
 	var place entity.Place
-	var crypto entity.Crypto
-	var bank entity.Bank
+	var method entity.Method
 
-	// ผลลัพธ์ที่ได้จากขั้นตอนที่ 10 จะถูก bind เข้าตัวแปร payment
+	// ผลลัพธ์ที่ได้จากขั้นตอนที่ 9 จะถูก bind เข้าตัวแปร payment
 	if err := c.ShouldBindJSON(&payment); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	// 11: ค้นหา paymentmethod ด้วย id
+	// 10: ค้นหา paymentmethod ด้วย id
 	if tx := entity.DB().Where("id = ?", payment.PaymentMethodID).First(&paymentmethod); tx.RowsAffected == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "paymentmethod not found"})
 		return
 	}
 
-	// 12: ค้นหา crypto ด้วย id
-	if tx := entity.DB().Where("id = ?", payment.CryptoID).First(&crypto); tx.RowsAffected == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "crypto not found"})
+	// 11: ค้นหา method ด้วย id
+	if tx := entity.DB().Where("id = ?", payment.MethodID).First(&method); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "method not found"})
 		return
 	}
 
-	// 13: ค้นหา bank ด้วย id
-	if tx := entity.DB().Where("id = ?", payment.BankID).First(&bank); tx.RowsAffected == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "bank not found"})
-		return
-	}
-
-	// 14: ค้นหา place ด้วย id
+	// 12: ค้นหา place ด้วย id
 	if tx := entity.DB().Where("id = ?", payment.PlaceID).First(&place); tx.RowsAffected == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "place not found"})
 		return
 	}
 
-	// 15: ค้นหา customer ด้วย id
+	// 13: ค้นหา customer ด้วย id
 	if tx := entity.DB().Where("id = ?", payment.CustomerID).First(&customer); tx.RowsAffected == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "customer not found"})
 		return
 	}
 
-	// 16: สร้าง Payment
+	// 14: สร้าง Payment
 	pm := entity.Payment{
 		Customer:      customer,
 		PaymentMethod: paymentmethod,
-		Crypto:        crypto,
-		Bank:          bank,
+		Method:        method,
 		Place:         place,
 		Time:          payment.Time,
 		Picture:       payment.Picture,
 	}
 
-	//17: save
+	//15: save
 	if err := entity.DB().Create(&pm).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
@@ -103,4 +95,50 @@ func ListPaymentByUID(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"data": payments})
+}
+
+// GET /paymentmethods
+func ListPaymentMethods(c *gin.Context) {
+	var paymentmethods []entity.PaymentMethod
+	if err := entity.DB().Raw("SELECT * FROM payment_methods").Find(&paymentmethods).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": paymentmethods})
+}
+
+// GET /methods/paymet/:id
+func ListMethodsByPID(c *gin.Context) {
+	var methods []entity.Method
+	id := c.Param("id")
+	if err := entity.DB().Raw("SELECT * FROM methods WHERE payment_method_id = ?", id).Find(&methods).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": methods})
+}
+
+// GET /method/:id
+func GetMethod(c *gin.Context) {
+	var methods entity.Method
+	id := c.Param("id")
+	if err := entity.DB().Preload("PaymentMethod").Raw("SELECT * FROM methods WHERE id = ?", id).Find(&methods).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": methods})
+}
+
+// GET /places
+func ListPlaces(c *gin.Context) {
+	var places []entity.Place
+	if err := entity.DB().Raw("SELECT * FROM places").Find(&places).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": places})
 }
