0a74ce7885e88b454b96c8b1c23d1ec87be8f704
Henlies <B6311032@g.sut.ac.th>
2023-02-10 10:28:47 +0700

    Validate Item and update controller - close #80

--- backend/entity/service_test.go
+++ backend/entity/service_test.go
@@ -13,58 +13,121 @@ func TestServiceValidate(t *testing.T) {
 
 	t.Run("Check Customer", func(t *testing.T) {
 		s := Service{
-			CustomerID:    0,
-			Time:          time.Now(),
-			FoodID:        1,
-			DrinkID:       1,
-			AccessoriesID: 1,
+			CustomerID:      0,
+			Time:            time.Now(),
+			FoodID:          1,
+			FoodItem:        1,
+			DrinkID:         1,
+			DrinkItem:       1,
+			AccessoriesID:   1,
+			AccessoriesItem: 1,
 		}
 
 		ok, err := govalidator.ValidateStruct(s)
 		g.Expect(ok).NotTo(BeTrue())
 		g.Expect(err).ToNot(BeNil())
-		g.Expect(err.Error()).To(Equal("Please login"))
+		g.Expect(err.Error()).To(Equal("Please Login"))
 	})
 	t.Run("Check Food", func(t *testing.T) {
 		s := Service{
-			CustomerID:    1,
-			Time:          time.Now(),
-			FoodID:        0,
-			DrinkID:       1,
-			AccessoriesID: 1,
+			CustomerID:      1,
+			Time:            time.Now(),
+			FoodID:          0,
+			FoodItem:        1,
+			DrinkID:         1,
+			DrinkItem:       1,
+			AccessoriesID:   1,
+			AccessoriesItem: 1,
 		}
 
 		ok, err := govalidator.ValidateStruct(s)
 		g.Expect(ok).NotTo(BeTrue())
 		g.Expect(err).ToNot(BeNil())
-		g.Expect(err.Error()).To(Equal("Please Choose some order of Food"))
+		g.Expect(err.Error()).To(Equal("Choose Food"))
 	})
 	t.Run("Check Drink", func(t *testing.T) {
 		s := Service{
-			CustomerID:    1,
-			Time:          time.Now(),
-			FoodID:        1,
-			DrinkID:       0,
-			AccessoriesID: 1,
+			CustomerID:      1,
+			Time:            time.Now(),
+			FoodID:          1,
+			FoodItem:        1,
+			DrinkID:         0,
+			DrinkItem:       1,
+			AccessoriesID:   1,
+			AccessoriesItem: 1,
 		}
 
 		ok, err := govalidator.ValidateStruct(s)
 		g.Expect(ok).NotTo(BeTrue())
 		g.Expect(err).ToNot(BeNil())
-		g.Expect(err.Error()).To(Equal("Please Choose some order of Drink"))
+		g.Expect(err.Error()).To(Equal("Choose Drink"))
 	})
 	t.Run("Check Accessories", func(t *testing.T) {
 		s := Service{
-			CustomerID:    1,
-			Time:          time.Now(),
-			FoodID:        1,
-			DrinkID:       1,
-			AccessoriesID: 0,
+			CustomerID:      1,
+			Time:            time.Now(),
+			FoodID:          1,
+			FoodItem:        1,
+			DrinkID:         1,
+			DrinkItem:       1,
+			AccessoriesID:   0,
+			AccessoriesItem: 1,
 		}
 
 		ok, err := govalidator.ValidateStruct(s)
 		g.Expect(ok).NotTo(BeTrue())
 		g.Expect(err).ToNot(BeNil())
-		g.Expect(err.Error()).To(Equal("Please Choose some order of Accessories"))
+		g.Expect(err.Error()).To(Equal("Choose Accessories"))
+	})
+	t.Run("Check Food Item", func(t *testing.T) {
+		s := Service{
+			CustomerID:      1,
+			Time:            time.Now(),
+			FoodID:          1,
+			FoodItem:        -1,
+			DrinkID:         1,
+			DrinkItem:       1,
+			AccessoriesID:   1,
+			AccessoriesItem: 1,
+		}
+
+		ok, err := govalidator.ValidateStruct(s)
+		g.Expect(ok).NotTo(BeTrue())
+		g.Expect(err).ToNot(BeNil())
+		g.Expect(err.Error()).To(Equal("FoodItem: -1 does not validate as range(0|50)"))
+	})
+	t.Run("Check Drink Item", func(t *testing.T) {
+		s := Service{
+			CustomerID:      1,
+			Time:            time.Now(),
+			FoodID:          1,
+			FoodItem:        1,
+			DrinkID:         1,
+			DrinkItem:       -1,
+			AccessoriesID:   1,
+			AccessoriesItem: 1,
+		}
+
+		ok, err := govalidator.ValidateStruct(s)
+		g.Expect(ok).NotTo(BeTrue())
+		g.Expect(err).ToNot(BeNil())
+		g.Expect(err.Error()).To(Equal("DrinkItem: -1 does not validate as range(0|50)"))
+	})
+	t.Run("Check Accessories Item", func(t *testing.T) {
+		s := Service{
+			CustomerID:      1,
+			Time:            time.Now(),
+			FoodID:          1,
+			FoodItem:        1,
+			DrinkID:         1,
+			DrinkItem:       1,
+			AccessoriesID:   1,
+			AccessoriesItem: -1,
+		}
+
+		ok, err := govalidator.ValidateStruct(s)
+		g.Expect(ok).NotTo(BeTrue())
+		g.Expect(err).ToNot(BeNil())
+		g.Expect(err.Error()).To(Equal("AccessoriesItem: -1 does not validate as range(0|50)"))
 	})
 }
