21b43e33e4c594817df29e400026af4e7cca61e6
Henlies <B6311032@g.sut.ac.th>
2023-02-21 05:44:19 +0700

    Update System - close #221

--- frontend/src/components/Services/ServiceShow.tsx
+++ frontend/src/components/Services/ServiceShow.tsx
@@ -1,4 +1,4 @@
-import { ButtonGroup, Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { ButtonGroup, Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from "@mui/material";
 import { ServicesInterface } from "../../models/modelService/IService";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { GetService } from "./service/ServiceHttpClientService";
@@ -23,6 +23,7 @@ const theme = createTheme({
 function ServiceShow() {
     const [service, setService] = useState<ServicesInterface[]>([]);
     const id_cus = localStorage.getItem("id");
+    const name = localStorage.getItem("name");
 
     const getservice = async () => {
         let res = await GetService(id_cus + "");
@@ -60,7 +61,18 @@ function ServiceShow() {
                         </Button>
                     </Grid>
                 </Grid>
-
+                <Grid container spacing={1} sx={{ padding: 3 }}>
+                    <Grid item xs={12}>
+                        <TextField
+                            style={{ float: "right" }}
+                            value={name + "'s Bill Service"}
+                            variant="standard"
+                            InputProps={{
+                                readOnly: true,
+                            }}
+                        />
+                    </Grid>
+                </Grid>
                 <div>
                     <Container maxWidth="xl">
                         <div style={{ height: 500, width: "100%", marginTop: "20px" }}>
@@ -69,7 +81,6 @@ function ServiceShow() {
                                     <TableHead>
                                         <TableRow>
                                             <TableCell align="center" width="20%">Bill</TableCell>
-                                            <TableCell align="center" width="20%">Customer</TableCell>
                                             <TableCell align="center" width="20%">Bill Time</TableCell>
                                             <TableCell align="center" width="20%">Food</TableCell>
                                             <TableCell align="center" width="20%">Item</TableCell>
@@ -77,6 +88,7 @@ function ServiceShow() {
                                             <TableCell align="center" width="20%">Item</TableCell>
                                             <TableCell align="center" width="20%">Accessorie</TableCell>
                                             <TableCell align="center" width="20%">Item</TableCell>
+                                            <TableCell align="center" width="20%">Total</TableCell>
                                             <TableCell align="center" width="20%">- Edit -</TableCell>
                                         </TableRow>
                                     </TableHead>
@@ -85,7 +97,6 @@ function ServiceShow() {
                                         {service.map((item: ServicesInterface) => (
                                             <TableRow key={item.ID}>
                                                 <TableCell align="center">{item.ID}</TableCell>
-                                                <TableCell align="center">{item.Customer?.FirstName}</TableCell>
                                                 <TableCell align="center">{moment(item.Time).format("DD/MM/YYYY HH:mm:ss")}</TableCell>
                                                 <TableCell align="center">{item.Food?.Name}</TableCell>
                                                 <TableCell align="center">{item.FoodItem}</TableCell>
@@ -93,6 +104,7 @@ function ServiceShow() {
                                                 <TableCell align="center">{item.DrinkItem}</TableCell>
                                                 <TableCell align="center">{item.Storage?.Product?.Name}</TableCell>
                                                 <TableCell align="center">{item.StorageItem}</TableCell>
+                                                <TableCell align="center">{item.Total}</TableCell>
                                                 <TableCell align="center">
                                                     <ButtonGroup color="primary" aria-label="outlined primary button group">
                                                         <Button
